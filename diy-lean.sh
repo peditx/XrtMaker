139dba8a8912ca8fba5b0995ad9e114e41f93136
PeDitX <pedram.ale@me.com>
2023-02-09 18:09:25 +0330

    Update diy-lean.sh

--- diy-lean.sh
+++ diy-lean.sh
@@ -12,7 +12,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
+git clone https://github.com/peditx/luci-theme-peditx package/luci-theme-peditx
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
