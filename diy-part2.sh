076963ef7805860f56979a7740fdc2386e21815c
PeDitX <pedram.ale@me.com>
2025-02-26 19:33:13 +0330

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,7 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+opkg remove luci-theme-bootstrap --force-depends
 
 # Change default IP to 10.1.1.1
 sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
